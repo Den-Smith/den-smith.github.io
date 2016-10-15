c5f0c0f991a1dd90cd518d82b926ca632f14c633
Den <denis.black.smith@yandex.ru>
2016-10-15 22:28:14 +0300

    add NurRemont

---
+++ NurRemont/js/main.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+
+// header scroll
+  $(window).scroll(function() {
+      if ($(window).scrollTop() > 800) {
+      	$('header').css({
+          	'background-color': '#3f5ab6',
+        });
+        $('.logo_wrap').css({
+        	'display': 'none',
+        });
+        $('.logo_wrap_small').css({
+        	'display': 'block',
+        });
+        $('.tel_wrap').css({
+        	'display': 'none',
+        });
+        $('.tel_wrap_small').css({
+        	'display': 'block',
+        });
+
+      } else{
+      	$('header').css({
+          	'background-color': 'transparent',
+        });
+    	$('.logo_wrap').css({
+    		'display': 'block',
+    	});
+    	$('.logo_wrap_small').css({
+    		'display': 'none',
+    	});
+    	$('.tel_wrap').css({
+          	'display': 'block',
+        });
+        $('.tel_wrap_small').css({
+        	'display': 'none',
+        });
+      }
+  });
+
+// slick gallery
+
+ $('.slick-gallery').slick({
+    dots: false,
+  	infinite: true,
+  	slidesToShow: 4,
+  	slidesToScroll: 4,
+  	responsive: [
+  	  {
+  	    breakpoint: 1200,
+  	    settings: {
+  	    	slidesToShow: 3,
+  	    	slidesToScroll: 3,
+  	    }
+  	  },
+  	  {
+  	    breakpoint: 992,
+  	    settings: {
+  	    	slidesToShow: 2,
+  	    	slidesToScroll: 2
+  	    }
+  	  },
+  	  {
+  	    breakpoint: 768,
+  	    settings: {
+  	    	slidesToShow: 1,
+  	    	slidesToScroll: 1
+  	    }
+  	  }
+  	]
+  	});
+
+ // feedback gallery
+
+ $('.slick-feedback').slick({
+    dots: false,
+  	infinite: true,
+  	slidesToShow: 3,
+  	slidesToScroll: 3,
+  	responsive: [
+  	  {
+  	    breakpoint: 1200,
+  	    settings: {
+  	    	slidesToShow: 3,
+  	    	slidesToScroll: 3,
+  	    }
+  	  },
+  	  {
+  	    breakpoint: 992,
+  	    settings: {
+  	    	slidesToShow: 2,
+  	    	slidesToScroll: 2
+  	    }
+  	  },
+  	  {
+  	    breakpoint: 768,
+  	    settings: {
+  	    	slidesToShow: 1,
+  	    	slidesToScroll: 1
+  	    }
+  	  }
+  	]
+  	});
+
+})
