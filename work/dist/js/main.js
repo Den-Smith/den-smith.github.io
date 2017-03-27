4095f25336c1aaf923ad13b210460271859b2e99
Den <denis.black.smith@yandex.ru>
2017-03-28 01:14:26 +0300

    re

--- work/dist/js/main.js
+++ work/dist/js/main.js
@@ -1,23 +1,24 @@
+
 $(document).ready(function(){
 
-	
-
-	$('.menu-btn').click(function(){
-		$(this).toggleClass('active');
-		$('.menu_container').toggleClass('active');
-		$('body').toggleClass('scroll')
+	$(window).on('load', function(){
+		$('.preloader').fadeOut('slow',function(){
+			$(this).remove();
+		});
 	});
-
-	function func() {
-		$('.description_item').addClass('animate');
-		$('.form_item').addClass('animate');
-	}
-
-	setTimeout(func, 800);
-
 	
-	// Preloader
-	$('.preloader').fadeOut();
+	 $('.menu-btn').click(function(){
+	 	$(this).toggleClass('active');
+	 	$('.menu_container').toggleClass('active');
+	 	$('body').toggleClass('scroll')
+	 });
+	
+	 function func() {
+	 	$('.description_item').addClass('animate');
+	 	$('.form_item').addClass('animate');
+	 }
+	
+	 setTimeout(func, 800);
 
 });
 
