996c0050c68578449943788c672b9b2bf8b0a866
Den <denis.black.smith@yandex.ru>
2017-03-30 14:47:16 +0300

    loans 1.0

--- work/dist/js/main.js
+++ work/dist/js/main.js
@@ -1,25 +1,37 @@
 
 $(document).ready(function(){
 
+	// preloader
 	$(window).on('load', function(){
 		$('.preloader').fadeOut('slow',function(){
 			$(this).remove();
 		});
 	});
-	
+
+	// menu button
 	 $('.menu-btn').click(function(){
 	 	$(this).toggleClass('active');
 	 	$('.menu_container').toggleClass('active');
 	 	$('body').toggleClass('scroll')
 	 });
 	
-	 function func() {
+	// animate hero section's items
+	 function animateForm() {
 	 	$('.description_item').addClass('animate');
 	 	$('.form_item').addClass('animate');
 	 }
-	
-	 setTimeout(func, 800);
+	 setTimeout(animateForm, 800);
 
+	 // footer disclaimer section + - change
+	 $('.footer_disclaimer .panel-heading a').click(function () {
+	 	$('.footer_disclaimer .panel-heading a').children().removeClass('fa-minus').addClass('fa-plus');
+	 	if ($(this).attr('aria-expanded') == 'true') {
+	 		$(this).children().removeClass('fa-minus').addClass('fa-plus');
+	 	} else{
+	 		$(this).children().removeClass('fa-plus').addClass('fa-minus');
+	 	}
+	 });
+	 
 });
 
 
