380588cd56ffc6dcd983ce81df620a39fcd7ba22
Den <denis.black.smith@yandex.ru>
2017-06-19 21:33:20 +0300

    re

--- js/main.js
+++ js/main.js
@@ -16,12 +16,12 @@ $(document).ready(function(){
 	$('a').attr('target', '_blank');
 	
 	// load imgs
-	[].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
-		img.setAttribute('src', img.getAttribute('data-src'));
-		img.onload = function() {
-			img.removeAttribute('data-src');
-		};
-	});
+	// [].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
+	// 	img.setAttribute('src', img.getAttribute('data-src'));
+	// 	img.onload = function() {
+	// 		img.removeAttribute('data-src');
+	// 	};
+	// });
 
 
 	// copy phone to buffer
