00f42754df2359df845f980133354025b25f38e2
Den <denis.black.smith@yandex.ru>
2017-08-23 17:37:31 +0300

    re

--- work/nails/dist/js/main.js
+++ work/nails/dist/js/main.js
@@ -35,6 +35,12 @@ $(document).ready(function(){
 			arrows: true
 	  });
 
+		$('.front-catalog--test-1').slick({
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			arrows: false
+	  });
+
 		$('.offers-nav li').click(function(event) {
 			event.preventDefault();
 			$('.offers-nav li').removeClass('active');
@@ -42,8 +48,34 @@ $(document).ready(function(){
 		});
 
 
-
-
+		$('.btn-minus').click(function () {
+			var $input = $(this).parent().find('input');
+			var count = parseInt($input.val()) - 1;
+			if (count <= 1) {
+				$(this).addClass('disabled')
+			} else {
+				$(this).removeClass('disabled')
+			}
+			console.log(count);
+			count = count < 1 ? 1 : count;
+			$input.val(count);
+			$input.change();
+			return false;
+		});
+		$('.btn-plus').click(function () {
+			var $input = $(this).parent().find('input');
+			$input.val(parseInt($input.val()) + 1);
+			if ($input <= 1) {
+				$(this).siblings('.btn-minus').addClass('disabled')
+			} else {
+				$(this).siblings('.btn-minus').removeClass('disabled')
+			}
+			$input.change();
+			return false;
+		});
+		$('.counter-number').click(function(event) {
+			$(this).select()
+		});
 
 
 
