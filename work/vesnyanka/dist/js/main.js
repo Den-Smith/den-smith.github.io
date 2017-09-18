fabeba91a4148e6990ac4e580afb72fda04eaca9
Den <denis.black.smith@yandex.ru>
2017-09-18 03:24:59 +0300

    re

--- work/vesnyanka/dist/js/main.js
+++ work/vesnyanka/dist/js/main.js
@@ -66,12 +66,65 @@ $(document).ready(function(){
 		$(this).addClass('active');
 	});
 
+	// fix header init
+	function headerResize() {
+		if (!$('html').hasClass('mobile') && $(window).innerWidth() > 768) {
+			$(window).scroll(function() {
+				if ($(window).scrollTop() > 0) {
+					$('header').addClass('fixed');
+				} else {
+					$('header').removeClass('fixed');
+				}
+			});
+		} else if($('html').hasClass('mobile')){
 
+		} else {
 
-	$('.our-production-slider .slider-item').click(function() {
-		$('.our-production-slider .slider-item').removeClass('hover')
-		$(this).addClass('hover');
+		};
+	}
+	headerResize();
+	$(window).resize(headerResize);
+
+	// menu slide's
+	$('.catalog-btn p').click(function(event) {
+		$('.slide-block-wrap.right-block').removeClass('in-active');
+		$('.slide-block-wrap.left-block').toggleClass('in-active');
+		if ($('.slide-block-wrap').hasClass('in-active')) {
+			$('body').addClass('no-scroll');
+		} else {
+			$('body').removeClass('no-scroll');
+		}
 	});
+	$('.collections-btn p').click(function(event) {
+		$('.slide-block-wrap.left-block').removeClass('in-active');
+		$('.slide-block-wrap.right-block').toggleClass('in-active');
+		if ($('.slide-block-wrap').hasClass('in-active')) {
+			$('body').addClass('no-scroll');
+		} else {
+			$('body').removeClass('no-scroll');
+		}
+	});
+	// close slide's
+		$('.slide-block-wrap .close-btn').click(function(event) {
+			$('.slide-block-wrap').removeClass('in-active');
+			$('body').removeClass('no-scroll');
+		});
+		$(document).mouseup(function (e){
+			if (!$('html').hasClass('mobile')) {
+				var divCollapse = $(".slide-block-wrap .slide-inner-wrap, .mobile-menu-wrap");
+				if (!divCollapse.is(e.target) && divCollapse.has(e.target).length === 0) {
+					$('body').removeClass('no-scroll');
+					$('.slide-block-wrap').removeClass('in-active');
+				}
+	    }
+		});
+
+
+
+	// $('.our-production-slider .slider-item').click(function() {
+	// 	$('.our-production-slider .slider-item').removeClass('hover')
+	// 	$(this).addClass('hover');
+	// });
 
 
 
